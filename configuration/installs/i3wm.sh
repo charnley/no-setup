6ef90d03d8da231a399e1df26339a8fee5e8c7b5
Jimmy Charnley Kromann <jimmy@charnley.dk>
2013-02-05 20:28:41 +0100

    okay, so this is how you install i3

--- configuration/installs/i3wm.sh
+++ configuration/installs/i3wm.sh
@@ -1,22 +1,7 @@
-#!/bin/bash
 
-# install for dependencies
-
-sudo apt-get install i3 
-sudo apt-get remove i3
-
-# But we are here for the newest
-
-# Automatic?
-# Download i3, i3status and i3lock
-wget http://code.stapelberg.de/git/i3/snapshot/i3-4.4.tar.gz
-wget http://code.stapelberg.de/git/i3status/snapshot/i3status-2.6.tar.gz
-wget http://code.stapelberg.de/git/i3lock/snapshot/i3lock-2.4.1.tar.gz
-tar -xf i3-4.4.tar.bz2
-tar -xf i3status-2.6.tar.gz
-tar -xf i3lock-2.4.1.tar.gz
-
-# Compile
-
-# Setup
+echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
+apt-get update
+apt-get --allow-unauthenticated install sur5r-keyring
+apt-get update
+apt-get install i3
 

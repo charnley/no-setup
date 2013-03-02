b81545bb3d1a7276a506459d37415ec9198f03c7
Jimmy Charnley Kromann, lat13 <jimmy@charnley.dk>
2013-03-02 12:36:32 +0100

    notes

---
+++ configuration/installs/dropbox.sh
@@ -0,0 +1,6 @@
+
+
+wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_1.4.0_amd64.deb
+sudo dpkg -i dropbox_1.4.0_amd64.deb
+
+
---
+++ configuration/installs/networkmanager.sh
@@ -0,0 +1,1 @@
+sudo apt-get install network-manager network-manager-gnome
